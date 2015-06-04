ae47cd2040d099d5ccc8647873cf87c9436df8b2
prashant <prashant1988mishra@gmail.com>
2015-06-04 17:51:16 +0530

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 Product.delete_all
 Product.create(:title => 'Programming Ruby 1.9' ,
-               :description =>
+               :describe =>
                    %{<p>
 Ruby is the fastest growing and most exciting dynamic language out
 there. If you need to get working programs delivered fast, you should
